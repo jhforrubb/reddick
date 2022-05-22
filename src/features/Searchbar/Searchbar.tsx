ac1fc62c36785598c1a1fad2c2a6decfbdeac618
drlappies <yau1chun2lap3@gmail.com>
2022-05-22 21:24:04 +0800

    search bar

--- src/features/Searchbar/Searchbar.tsx
+++ src/features/Searchbar/Searchbar.tsx
@@ -1,6 +1,9 @@
 import './searchbar.scss';
+import { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
+import SearchIcon from '@mui/icons-material/Search';
+import ClearIcon from '@mui/icons-material/Clear';
 
 const sampleJson = [
     { id: 0, label: "search result 1" },
@@ -12,13 +15,19 @@ const sampleJson = [
 ]
 
 const Searchbar = () => {
+    const [isFocus, setIsFocus] = useState<boolean>(false);
+
     return (
-        <div className="searchbar">
-            <Autocomplete
-                freeSolo={true}
-                options={sampleJson.map(el => el.label)}
-                renderInput={(params) => <TextField {...params} label="Search Reddit" />}
+        <div className={`searchbar-container ${isFocus ? "focused" : ""}`}>
+            <SearchIcon className="search-icon" />
+            <input
+                className="search-input"
+                type="text"
+                placeholder='Search Reddit'
+                onFocus={() => setIsFocus(true)}
+                onBlur={() => setIsFocus(false)}
             />
+            <ClearIcon className="search-icon" />
         </div>
     )
 }
