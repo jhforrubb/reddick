6e9b49725c6aa8a9b8f3db90dd5f702bbcabd236
drlappies <yau1chun2lap3@gmail.com>
2022-05-23 21:15:56 +0800

    trending card

--- src/features/TrendingBanner/TrendingBanner.tsx
+++ src/features/TrendingBanner/TrendingBanner.tsx
@@ -1,9 +1,21 @@
 import './trending-banner.scss';
+import TrendingCard from './TrendingCard';
+
+const sampleJson = [
+    { id: 0, title: "Premier League", subtitle: "[Official] Manchester City are the 2021/22 Premier League Champions", sub: "r/soccer and more" },
+    { id: 1, title: "Baby Formula", subtitle: "Jen Psaki Says Biden Administration Providing Migrant Infants Baby Formula amid Shortage Is 'Morally Right'", sub: "r/politics and more" },
+    { id: 2, title: "Max Verstappen", subtitle: "Max Verstappen wins the 2022 Spanish Grand Prix", sub: "r/formula1 and more" },
+    { id: 3, title: "Ukraine", subtitle: "Zelensky says Russia has blocked Ukraine from exporting 22M tons of food products", sub: "" },
+]
+
 
 const TrendingBanner = () => {
     return (
         <div className="trending-banner-root">
             <div className="trending-banner-header">Trending Today</div>
+            <div className="trending-banner-slide">
+                {sampleJson.map(el => <TrendingCard key={el.id} title={el.title} subtitle={el.subtitle} sub={el.sub} />)}
+            </div>
         </div>
     )
 }
--- src/features/TrendingBanner/TrendingCard.tsx
+++ src/features/TrendingBanner/TrendingCard.tsx
@@ -1,8 +1,25 @@
+import './trending-banner.scss';
 
+type TrendingCardProps = {
+    title: string,
+    subtitle: string,
+    sub?: string,
+    imgUrl?: string,
+}
 
-const TrendingCard = () => {
+const TrendingCard = (props: TrendingCardProps) => {
+    const { title, subtitle, sub, imgUrl } = props;
+
     return (
-        <div></div>
+        <div className="trending-card-root">
+            <img alt={imgUrl} src={imgUrl} />
+
+            <div className="trending-card-info">
+                <div className="trending-card-title">{title}</div>
+                <div className="trending-card-subtitle">{subtitle}</div>
+                <div className="trending-card-sub">{sub}</div>
+            </div>
+        </div>
     )
 }
 
