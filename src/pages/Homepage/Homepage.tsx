bfe63298bbda0497652622db876e9704ef60d1f0
drlappies <yau1chun2lap3@gmail.com>
2022-05-23 20:35:05 +0800

    page centering

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,7 +1,11 @@
+import './homepage.scss'
+
 const Homepage = () => {
     return (
-        <div>
-            <h1 >home page</h1>
+        <div className="homepage-container">
+            <div className="homepage-container-body">
+                <h1 >home page</h1>
+            </div>
         </div>
     );
 };
