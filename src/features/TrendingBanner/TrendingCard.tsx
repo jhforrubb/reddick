6c26a6f7ba7a45114bd0e7ef13848bf266e00986
drlappies <yau1chun2lap3@gmail.com>
2022-05-24 10:44:16 +0800

    fix trending posts banner with chakra ui

--- src/features/TrendingBanner/TrendingCard.tsx
+++ src/features/TrendingBanner/TrendingCard.tsx
@@ -1,4 +1,4 @@
-import './trending-banner.scss';
+import { Box, Image, Text } from '@chakra-ui/react';
 
 type TrendingCardProps = {
     title: string,
@@ -11,15 +11,21 @@ const TrendingCard = (props: TrendingCardProps) => {
     const { title, subtitle, sub, imgUrl } = props;
 
     return (
-        <div className="trending-card-root">
-            <img alt={imgUrl} src={imgUrl} />
-
-            <div className="trending-card-info">
-                <div className="trending-card-title">{title}</div>
-                <div className="trending-card-subtitle">{subtitle}</div>
-                <div className="trending-card-sub">{sub}</div>
-            </div>
-        </div>
+        <Box
+            w="240px"
+            h="185px"
+            position="relative"
+            borderWidth="1px"
+            borderStyle="solid"
+            borderRadius="15px"
+            background={`url(${imgUrl}) center center / cover no-repeat #FFFFFF;`}
+        >
+            <Box position="absolute" px="2.5" py="2.5" bottom="0">
+                <Text fontSize="md" color="#FFFFFF" fontWeight="500">{title}</Text>
+                <Text fontSize="sm" color="#FFFFFF" fontWeight="400" maxH="40px" textOverflow="ellipsis" overflow="hidden">{subtitle}</Text>
+                <Text fontSize="xs" color="#FFFFFF" minH="18px">{sub}</Text>
+            </Box>
+        </Box >
     )
 }
 
