9d1f19a6c792c1f078ff42ff00e784d2f688e15c
drlappies <yau1chun2lap3@gmail.com>
2022-05-24 20:33:52 +0800

    page basic layout setup

---
+++ src/features/PostFilter/PostFilter.tsx
@@ -0,0 +1,9 @@
+import { Box } from '@chakra-ui/react'
+
+const PostFilter = () => {
+    return (
+        <Box>POST FILTER HERE</Box>
+    )
+}
+
+export default PostFilter
--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,10 +1,12 @@
 import { Box } from '@chakra-ui/react'
 import TrendingBanner from '../../features/TrendingBanner/TrendingBanner';
+import MainLayout from '../../layouts/MainLayout';
 
 const Homepage = () => {
     return (
         <Box pt="50px" height="100vh">
             <TrendingBanner />
+            <MainLayout primaryView={<div>MAIN CONTAINER</div>} secondaryView={<div>SECONDARY CONTAINER</div>} />
         </Box>
     );
 };
