b710542885daef0dd66db84903acf67fa60a30ba
Jennifer Ho <jenh.yy12@gmail.com>
2022-05-25 18:37:57 +0800

    add top communities

---
+++ src/features/TopCommunities/TopCommunities.tsx
@@ -0,0 +1,104 @@
+import React from 'react';
+import { Box, Image, Flex, HStack, Spacer, Button, Center, Skeleton, SkeletonCircle, Stack } from '@chakra-ui/react';
+import { ChevronUpIcon } from '@chakra-ui/icons';
+
+type TopCommunitiesProps = {
+    isLoading: boolean;
+    imgUrl?: string;
+    communities: Array<Community>;
+};
+
+type Community = {
+    id: number;
+    trending: boolean;
+    icon: string;
+    r: string;
+};
+
+const TopCommunities = (props: TopCommunitiesProps) => {
+    const { isLoading, imgUrl, communities } = props;
+
+    const renderSkeleton = () => {
+        return (
+            <>
+                <Skeleton h="50px" />
+                {[...Array(5)].map((value: undefined, index: number) => (
+                    <Box padding="7px" borderBottom="1px" borderColor="gray.200" key={index}>
+                        <HStack>
+                            <SkeletonCircle size="10" />
+                            <Skeleton h="30px" w="250px" />
+                        </HStack>
+                    </Box>
+                ))}
+            </>
+        );
+    };
+
+    const renderInfo = () => {
+        return (
+            <>
+                <Flex
+                    h="80px"
+                    direction="column-reverse"
+                    padding="15px"
+                    color="white"
+                    fontWeight="medium"
+                    backgroundPosition="50%"
+                    bgImage={imgUrl}
+                >
+                    Top News Communities
+                </Flex>
+                {/* bgImage={`linear-gradient(0deg,rgba(0,0,0,.7) 0,transparent,url(${imgUrl}))`} */}
+                {/* <Image src={imgUrl} alt={imgAlt} objectFit='none'/>             */}
+
+                <Box>
+                    {communities.map((el) => (
+                        <Flex padding="10px" borderBlock="1px" borderColor="gray.300" fontSize="small" fontWeight="semibold">
+                            <HStack>
+                                <Box w="10px">{el.id + 1}</Box>
+                                <Box w="20px">{el.trending ? <ChevronUpIcon boxSize="1.5em" color="green.400" /> : ''}</Box>
+                                <Box>
+                                    <Image borderRadius="full" boxSize="30px" src={el.icon} alt="comuIcon" />
+                                </Box>
+                                <Box>{el.r}</Box>
+                            </HStack>
+                            <Spacer />
+                            <Box>
+                                <Button colorScheme="blue" size="sm" borderRadius="20px" height="25px" fontSize="xs">
+                                    Join
+                                </Button>
+                            </Box>
+                        </Flex>
+                    ))}
+                </Box>
+
+                <Center paddingBlock="12px">
+                    <Button colorScheme="blue" size="md" borderRadius="20px" height="30px" width="280px">
+                        View All
+                    </Button>
+                </Center>
+
+                <Box padding="0 8px 12px">
+                    <Button colorScheme="gray" size="sm" fontSize="smaller" borderRadius="15px" margin='4px'>
+                        Top
+                    </Button>
+                    <Button colorScheme="gray" size="sm" fontSize="smaller" borderRadius="15px" margin='4px'>
+                        Near You
+                    </Button>
+                    <Button colorScheme="gray" size="sm" fontSize="smaller" borderRadius="15px" margin='4px'>
+                        Sports
+                    </Button>
+                </Box>
+            </>
+        );
+    };
+
+    return (
+        <Box border="1px" borderRadius="5px" borderColor="gray.300" bgColor="rgba(255,255,255)" overflow="hidden">
+            {isLoading ? renderSkeleton() : renderInfo()}
+        </Box>
+    );
+
+};
+
+export default TopCommunities;
--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,13 +1,46 @@
-import { Box } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react';
 import TrendingBanner from '../../features/TrendingBanner/TrendingBanner';
 import MainLayout from '../../layouts/MainLayout';
-import Post from '../../features/Post/Post'
+import Post from '../../features/Post/Post';
+import TopCommunities from '../../features/TopCommunities/TopCommunities';
 
 const Homepage = () => {
+    const communitiesJson = [
+        { id: 0, trending: true, icon:'https://styles.redditmedia.com/t5_2qh3l/styles/communityIcon_fmygcobc22z81.png',r: 'r/news' },
+        { id: 1, trending: true, icon:'https://styles.redditmedia.com/t5_2qi4j/styles/communityIcon_a0b0l0lb75k41.png',r: 'r/technews' },
+        { id: 2, trending: false,icon:'https://styles.redditmedia.com/t5_323r3/styles/communityIcon_f8aq15b6x9t21.png', r: 'r/savedyouaclick' },
+        { id: 3, trending: true,icon:'https://styles.redditmedia.com/t5_21of/styles/communityIcon_s3acgrjuyoe81.jpg?format=pjpg&s=6be19403d863b791c4c4e87db4138533f8733b9f', r: 'r/olympics' },
+
+    ];
+
     return (
         <Box pt="50px" height="100vh">
             <TrendingBanner />
-            <MainLayout primaryView={<div><Post subredditIconUrl="https://a.thumbs.redditmedia.com/kIpBoUR8zJLMQlF8azhN-kSBsjVUidHjvZNLuHDONm8.png" voteCount={100} subreddit={"r/post"} user={"myau123"} timestamp={new Date().toISOString()} isJoined={false} title={"Better Call Saul S06E07 - [Mid-Season Finale] Plan and Execution - Post-Episode Discussion Thread"} commentCount={50} /> </div>} secondaryView={<div>SECONDARY CONTAINER</div>} />
+            <MainLayout
+                primaryView={
+                    <div>
+                        <Post
+                            subredditIconUrl="https://a.thumbs.redditmedia.com/kIpBoUR8zJLMQlF8azhN-kSBsjVUidHjvZNLuHDONm8.png"
+                            voteCount={100}
+                            subreddit={'r/post'}
+                            user={'myau123'}
+                            timestamp={new Date().toISOString()}
+                            isJoined={false}
+                            title={'Better Call Saul S06E07 - [Mid-Season Finale] Plan and Execution - Post-Episode Discussion Thread'}
+                            commentCount={50}
+                        />{' '}
+                    </div>
+                }
+                secondaryView={
+                    <div>
+                        <TopCommunities
+                            imgUrl="https://www.redditstatic.com/desktop2x/img/leaderboard/banner-background.png"
+                            communities={communitiesJson}
+                            isLoading={false}
+                        />
+                    </div>
+                }
+            />
         </Box>
     );
 };
