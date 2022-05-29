3325803e600a797781ae3ed617f53b89ae3dec0b
drlappies <yau1chun2lap3@gmail.com>
2022-05-29 14:07:58 +0800

    add login modal

--- src/contexts/ModalContext.tsx
+++ src/contexts/ModalContext.tsx
@@ -1,24 +1,32 @@
 import { createContext, useState } from 'react';
+import LoginModal from '../features/Modal/LoginModal';
 
-export interface ModalContextInterface {
-    isModalOpen: boolean;
-    toggleModal: () => void;
+export enum ModalType {
+    CLOSED = 'closed',
+    LOGIN = 'login',
+    SIGN_IN = 'sign_in',
 }
 
 export interface ModalContextProps {
     children: React.ReactNode;
 }
 
+export interface ModalContextInterface {
+    currentModal: ModalType;
+    setCurrentModal: React.Dispatch<React.SetStateAction<ModalType>>;
+}
+
 export const ModalContext = createContext({} as ModalContextInterface);
 
-const ModalContextProvider = (props: ModalContextProps) => {
-    const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+export const ModalContextProvider = (props: ModalContextProps) => {
+    const [currentModal, setCurrentModal] = useState<ModalType>(ModalType.CLOSED);
 
-    const toggleModal = () => {
-        setIsModalOpen(true);
-    };
-
-    return <ModalContext.Provider value={{ isModalOpen, toggleModal }}>{props.children}</ModalContext.Provider>;
+    return (
+        <ModalContext.Provider value={{ currentModal, setCurrentModal }}>
+            {props.children}
+            <LoginModal isOpen={currentModal === ModalType.LOGIN} />
+        </ModalContext.Provider>
+    );
 };
 
 export default ModalContextProvider;
