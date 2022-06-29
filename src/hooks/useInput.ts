4127ebbc36f9965838cceb7608487fd3f8654cde
drlappies <yau1chun2lap3@gmail.com>
2022-06-29 22:04:38 +0800

    useInput hook

---
+++ src/hooks/useInput.ts
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+function useInput(initialValue = '') {
+    const [value, setValue] = useState(initialValue);
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => setValue(event.target.value);
+
+    const handleSet = (str: string) => setValue(str);
+
+    return [value, handleChange, handleSet];
+}
+
+export default useInput;
