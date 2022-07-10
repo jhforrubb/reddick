decb2fedfc5f1725ab17ea46d52a719c198f1c68
Jennifer Ho <jenh.yy12@gmail.com>
2022-07-10 16:48:07 +0800

    fix image post

--- src/features/Post/ImagePost.tsx
+++ src/features/Post/ImagePost.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, BaseSyntheticEvent } from 'react';
+import { BaseSyntheticEvent, useRef } from 'react';
 import { Box, Image } from '@chakra-ui/react';
 
 type ImagePostProps = {
@@ -7,47 +7,48 @@ type ImagePostProps = {
 
 const ImagePost = (props: ImagePostProps) => {
     const { content } = props;
-    const [isHigherThan1000px, setIsHigherThan1000px] = useState<boolean>(false);
+    const imageRef = useRef<HTMLImageElement>(null);
+    const buttonRef = useRef<HTMLDivElement>(null);
 
     const handleImageLoad = (e: BaseSyntheticEvent) => {
+        if (!imageRef.current || !buttonRef.current) return;
+
         if (e.currentTarget.naturalHeight > 1000) {
-            setIsHigherThan1000px(true);
+            imageRef.current.style.maxHeight = 'none';
+            buttonRef.current.style.display = 'block';
+            return;
         }
+
+        imageRef.current.style.maxHeight = '512px';
+        buttonRef.current.style.display = 'none';
     };
-
-    const renderZoomBtn = useCallback(() => {
-        if (isHigherThan1000px) {
-            return (
-                <Box
-                    position="absolute"
-                    bottom="15"
-                    fontSize="12px"
-                    fontWeight="700"
-                    letterSpacing="0.5px"
-                    textTransform="uppercase"
-                    borderRadius="4px"
-                    lineHeight="32px"
-                    bgColor="rgba(80, 85, 87, 0.8)"
-                    color="#fff"
-                    textAlign="center"
-                    width="320px"
-                    userSelect="none"
-                    cursor="pointer"
-                >
-                    see full image
-                </Box>
-            );
-        }
-
-        return null;
-    }, [isHigherThan1000px]);
 
     return (
         <Box maxH="512px" overflow="hidden" display="flex" justifyContent="center" position="relative">
             <Box>
-                <Image onLoad={handleImageLoad} src={content} alt={content} maxH={isHigherThan1000px ? 'none' : '512px'} />
+                <Image ref={imageRef} onLoad={handleImageLoad} src={content} alt={content} />
             </Box>
-            {renderZoomBtn()}
+            <Box
+                ref={buttonRef}
+                as="div"
+                display={'none'}
+                position="absolute"
+                bottom="15"
+                fontSize="12px"
+                fontWeight="700"
+                letterSpacing="0.5px"
+                textTransform="uppercase"
+                borderRadius="4px"
+                lineHeight="32px"
+                bgColor="rgba(80, 85, 87, 0.8)"
+                color="#fff"
+                textAlign="center"
+                width="320px"
+                userSelect="none"
+                cursor="pointer"
+            >
+                see full image
+            </Box>
         </Box>
     );
 };
